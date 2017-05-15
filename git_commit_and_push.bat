6752cd354a2635d6a4f9dc942f30fe35927e6d3c
p0te <mjbez@outlook.com>
2017-05-15 10:47:55 +0200

    made changes: .bat commit

--- git_commit_and_push.bat
+++ git_commit_and_push.bat
@@ -1,6 +1,7 @@
 cd  C:\akademie\EERI418
 set HOME=%USERPROFILE%
 git config credential.https://github.com.username myusername
+git config credential.https://github.com.password Allekafferssalgooi
 git add .
 git commit -am "made changes: .bat commit"
 git push
